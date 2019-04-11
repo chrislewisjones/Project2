2fc71b55a2af3b2c8160461acce67cd936f3ce38
chrislewisjones <47401704+chrislewisjones@users.noreply.github.com>
2019-04-11 13:37:25 -0500

    heroku deployment

--- controller/html-routes.js
+++ controller/html-routes.js
@@ -7,6 +7,6 @@ module.exports = function(app) {
 
   // index route loads view.html
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/blog.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 };
