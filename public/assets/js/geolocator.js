06eb8d81299d973957e28f82c4dbc851302207a8
chrislewisjones <47401704+chrislewisjones@users.noreply.github.com>
2019-04-15 17:30:32 -0500

    a lot of appending

--- public/assets/js/geolocator.js
+++ public/assets/js/geolocator.js
@@ -14,9 +14,19 @@ $(".geolocator").click(function() {
 function getNearbyLocations(latitude, longitude) {
   $.post("/api/geolocation", { latitude, longitude }, function(response) {
     console.log("nearby:", response);
-    var item = $("<div>");
-    item.attr("id", "barName");
-    item.text(response[0].name);
-    $("#yelpResults").append(item);
+    for (let i = 0; i < response.length; i++) {
+      var item = $("<div>");
+      item.html(
+        `<div class="card" style="width: 18rem;"><img src=${
+          response[i].image_url
+        } class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">${
+          response[i].name
+        }</h5><p class="card-text">${response[i].location.address1}
+        <br>${
+          response[i].location.city
+        }</p><a href="#" class="btn btn-primary">Leave Review</a><button class="btn btn-success">Read Reviews</button></div></div>`
+      );
+      $("#yelpResults").append(item);
+    }
   });
 }
--- utils/yelp.js
+++ utils/yelp.js
@@ -13,26 +13,5 @@ exports.queryYelpNearby = function(latitude, longitude) {
     // coordinates: {
     latitude: latitude,
     longitude: longitude
-    // }
   });
-  // .then(response => {
-  //   console.log(response.jsonBody.businesses[i].name);
-  // })
-  // .then(function(response) {
-  //   return new Promise(function(resolve) {
-  //     return resolve(response.jsonBody.businesses);
-  //   });
-
-  // var pOne = document.createElement("<div>");   // Create a <button> element
-  // item.innerHTML = "CLICK ME";                   // Insert text
-  // document.body.appendChild();
-
-  // var item = $("<div>");
-  // item.attr("id", "barName");
-  // item.text(response.jsonBody.businesses.name);
-  // $("#yelpResults").append(item);
-  // })
-  // .catch(e => {
-  //   console.log(e);
-  // });
 };
