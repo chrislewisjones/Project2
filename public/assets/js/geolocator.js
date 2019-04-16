783359aef65f4482296205d284335b87e2beba9a
chrislewisjones <47401704+chrislewisjones@users.noreply.github.com>
2019-04-16 16:04:30 -0500

    update clear

--- public/assets/js/geolocator.js
+++ public/assets/js/geolocator.js
@@ -11,6 +11,7 @@ $(".geolocator").click(function() {
 
 function getNearbyLocations(latitude, longitude) {
   $.post("/api/geolocation", { latitude, longitude }, function(response) {
+    $("#yelpResults").empty();
     console.log("nearby:", response);
     for (let i = 0; i < response.length; i++) {
       var item = $("<div>");
@@ -20,11 +21,13 @@ function getNearbyLocations(latitude, longitude) {
         } class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">${
           response[i].name
         }</h5><p class="card-text">${response[i].location.address1}
-        <br>${
-          response[i].location.city
-
-        }</p><a href=/review?data=${response[i].name.split(" ").join("%")} class="btn btn-primary" >Leave Review</a><a href="/readreview" class="btn btn-success">Read Reviews</a></div></div>`
-
+        <br>${response[i].location.city}</p><a href=/review?data=${response[
+          i
+        ].name
+          .split(" ")
+          .join(
+            "%"
+          )} class="btn btn-primary" >Leave Review</a><a href="/readreview" class="btn btn-success">Read Reviews</a></div></div>`
       );
       $("#yelpResults").append(item);
     }
