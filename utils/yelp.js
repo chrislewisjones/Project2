fabc8363a4c5f19fcd3dd57734f1c8f9546204b4
irfanp90 <irfanp90@gmail.com>
2019-04-16 11:19:03 -0500

    update

--- utils/yelp.js
+++ utils/yelp.js
@@ -8,7 +8,7 @@ exports.queryYelpNearby = function(latitude, longitude) {
     term: '"dogs allowed"',
     categories: "bars",
     radius: 1610,
-    limit: 5,
+    limit: 10,
     // location: "Chicago, IL"
     // coordinates: {
     latitude: latitude,
