a47b93ac38f41deba7d402dfd03daf1e0ee06a6e
irfanp90 <irfanp90@gmail.com>
2019-04-16 21:16:30 -0500

    update

--- public/assets/js/review.js
+++ public/assets/js/review.js
@@ -23,7 +23,7 @@ $(document).ready(function() {
       bar_name: barInput.val().trim(),
       comment: commentInput.val().trim(),
       rate: ratingInput,
-      created_at: moment().format("YYYY-MM-DD HH:mm:ss")
+      created_at: moment().format("YYYY-MM-DD HH:mm:ss") 
     };
 
     console.log(newReview);
@@ -34,8 +34,8 @@ $(document).ready(function() {
       !commentInput.val().trim() ||
       !ratingInput
     ) {
-      //  return  $("#myModal").modal("toggle");
-      alert("please enter the fields");
+       return  $("#myModal").modal("toggle");
+      // alert("please enter the fields");
     }
     // Send an AJAX POST-request with jQuery
     $.post("/api/new", newReview)
